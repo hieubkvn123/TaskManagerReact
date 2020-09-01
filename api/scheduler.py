f8ff3decb239fe697311b44b821e4fde64e8a2dc
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-09-01 21:53:09 +0800

    ahihi

---
+++ api/scheduler.py
@@ -0,0 +1,43 @@
+from flask import Blueprint
+from flask import request
+from flask_cors import CORS
+import pymongo
+
+scheduler = Blueprint("scheduler", __name__, template_folder='templates')
+CORS(scheduler)
+
+### Create a mongo client ###
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client['productivity']
+
+'''
+    Inside of Schedule model :
+    1. Schedule name
+    2. Type : (Default, Non-default)
+    3. If non-default, from which time to which time 
+'''
+@scheduler.route('/create', methods=['POST'])
+def create_schedule():
+    if request.method == 'POST':
+        my_docs = {}
+        my_collection = db['Schedules']
+    
+        my_docs['name'] = request.form['name']
+        my_docs['type'] = request.form['type']
+
+        ### If this is not a default schedule ###
+        if(request.form['type'] != 'default'):
+            my_docs['period']  = request.form['period']
+        else: ### If this is a default schedule ### 
+            ### Check if there is any default schedules ###
+            count = my_collection.count_documents(filter={})
+            if(count >= 1):
+                print('Cannot insert another default schedule')
+                return 'default_exists'
+            
+        result = my_collection.insert_one(my_docs)
+
+        if(result.inserted_id):
+            return 'success'
+        else:
+            return 'fail'
---
+++ api/server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+# from history import history 
+from scheduler import scheduler
+
+PORT = 8080
+app = Flask(__name__)
+
+'''
+    1. Create and manage schedules
+    2. Among schedules there will be default and side schedules
+    3. In each schedule, there will be activities and timeslots
+    4. Each schedule activity is assigned a priority
+'''
+#app.register_blueprint(History)
+app.register_blueprint(scheduler, url_prefix='/scheduler')
+CORS(app)
+
+@app.route('/')
+def home():
+    return "<h1>Hello World</h1>"
+
+if __name__ == '__main__':
+    app.run(port=PORT, host='0.0.0.0', debug=True)
