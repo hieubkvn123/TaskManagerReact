4b8230ce04d8af4c9371112f664657d23fe79cab
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-09-04 14:11:50 +0800

    Added a nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CreateSchedule from './home/create_schedule'
-
+import ScheduleNavbar from './home/navbar'
 function App() {
   return (
     <div className="App">
 	  	<div class='bg'></div>
+	  	<ScheduleNavbar/>
     	<CreateSchedule/>
 	</div>
   );
---
+++ client/src/home/navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import { 
+	Nav,
+	Form,
+	FormControl,
+	Button
+} from 'react-bootstrap'
+
+class ScheduleNavbar extends Component {
+	constructor(props){
+		super(props)
+		this.state = {}
+	}
+
+	componentWillMount() {
+
+	}
+
+	componentWillUnmount() {
+
+	}
+
+	render = () => {
+		return (
+			<div id='schedule-navbar'>
+			  <Navbar bg="dark" variant="dark">
+				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+				<Nav className="mr-auto">
+				  <Nav.Link href="#home">Home</Nav.Link>
+				  <Nav.Link href="#features">Features</Nav.Link>
+				  <Nav.Link href="#pricing">Pricing</Nav.Link>
+				</Nav>
+				<Form inline>
+				  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+				  <Button variant="outline-info">Search</Button>
+				</Form>
+			  </Navbar>
+			</div>
+		)
+	}
+}
+
+export default ScheduleNavbar;
