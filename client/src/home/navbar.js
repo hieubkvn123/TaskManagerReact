37e29b9bee859fedc276fe65b95876dc219e82d4
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-09-04 14:48:59 +0800

    ahihi

--- client/src/home/navbar.js
+++ client/src/home/navbar.js
@@ -25,7 +25,7 @@ class ScheduleNavbar extends Component {
 		return (
 			<div id='schedule-navbar'>
 			  <Navbar bg="dark" variant="dark">
-				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+				<Navbar.Brand href="#home"></Navbar.Brand>
 				<Nav className="mr-auto">
 				  <Nav.Link href="#home">Home</Nav.Link>
 				  <Nav.Link href="#features">Features</Nav.Link>
